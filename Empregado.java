e18247f8adbf54181684fc1b912b252d443b90bb
Vitor Cinosi <vitorcinosi@outlook.com>
2021-03-23 00:10:50 -0300

    Calculo de salario realizado em função do tipo do empregado

---
+++ Empregado.java
@@ -0,0 +1,35 @@
+public class Empregado{
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+    private String nome;
+    private int idade;
+
+    public double calcularSalario() {
+        switch (tipo) {
+            case 1:
+            return salario;
+            case 2:
+            return salario + salario * comissao;
+            case 3:
+            return salario + bonus;
+            default:
+                return 0;
+        }
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
